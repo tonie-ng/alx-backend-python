a8621b69360b30048a76eb802e731d6c6390215e
Tonie Victor <victornwanochi@gmail.com>
2024-06-12 04:25:18 +0000

    Run time for four parallel comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Task 1"""
+
+from typing import List
+from importlib import import_module as using
+
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Creates a list of 10 numbers.
+    """
+    return [num async for num in async_generator()]
